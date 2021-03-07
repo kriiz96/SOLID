48ff71ec7f12ce96e49b23073c61daf2ea4498c0
kriiz96 <63168570+kriiz96@users.noreply.github.com>
2021-03-07 17:48:02 -0600

    Add files via upload

---
+++ Motorcycle.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Motorcycle extends Vehicle {
+
+    /**
+     * Default constructor
+     */
+    public Motorcycle() {
+    }
+
+    /**
+     * @return
+     */
+    public void Drive() {
+        System.out.println("It are motorcycle and can move forward using your 2 tires");
+    }
+
+}
---
+++ Vehicle.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Vehicle {
+
+    /**
+     * Default constructor
+     */
+    public Vehicle() {
+    }
+
+    /**
+     * @return
+     */
+    public void Drive() {
+        System.out.printl("The vehicle can move forward");
+    }
+
+}
