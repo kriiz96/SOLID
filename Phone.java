c517afb3b12c6f33712be9df960c9cbeafa4c6e0
kriiz96 <63168570+kriiz96@users.noreply.github.com>
2021-03-07 17:49:22 -0600

    Add files via upload

---
+++ Phone.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Phone extends Phone {
+
+    /**
+     * Default constructor
+     */
+    public Phone() {
+    }
+
+}
---
+++ SmartPhone.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class SmartPhone extends Phone {
+
+    /**
+     * Default constructor
+     */
+    public SmartPhone() {
+    }
+
+}
